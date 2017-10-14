494ced328af5b8bd1218c4ff7acc269a822bf25c
Chris McKinlay <chris.mckinlay@gmail.com>
2017-10-14 09:53:07 -0700

    add skeleton for cmd line parser

--- src/Development/Stake.hs
+++ src/Development/Stake.hs
@@ -1,12 +1,70 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Development.Stake (main) where
 
+import Data.Monoid ((<>))
 import Development.Shake hiding (command)
 import Development.Stake.Build
 import Development.Stake.Core
 import Development.Stake.Package
 import Development.Stake.Stackage
 import Distribution.Package
+import Options.Applicative
+
+
+data Command = Clean | CleanAll | Build PlanName PackageName
+
+
+cleanCommand :: Parser Command
+cleanCommand = flag' undefined
+  (  long "clean"
+  <> short 'c'
+  <> help "Clean project" )
+
+cleanAllCommand :: Parser Command
+cleanAllCommand = flag' undefined
+  (  long "clean-all"
+  <> help "Clean project & dependencies" )
+
+buildCommand :: Parser Command
+buildCommand = Build <$> planName <*> packageName
+  where
+    planName = PlanName <$> strOption
+      (  long "build"
+      <> short 'b'
+      <> metavar "PLANNAME"
+      <> help "Build project" )
+    packageName = PackageName <$> strOption
+      (  long "build"
+      <> short 'f'
+      <> metavar "PACKAGENAME"
+      <> help "Build project" )
+
+
+input :: Parser Command
+input = cleanCommand  <|> cleanAllCommand <|> buildCommand
+
+opts :: ParserInfo Command
+opts = info input mempty
+
+runWithOptions :: ParserResult Command -> Rules ()
+runWithOptions = undefined
+
+main :: IO ()
+main = runStake $ \args -> do
+    downloadCabalPackageRule
+    buildPlanRules
+    buildPackageRules
+    runWithOptions $ execParserPure undefined opts args
+
+{-
+ -
+sample :: Parser Command
+sample = subparser
+       ( command "clean" undefined
+      <> command "clean-all" undefined
+      <> command "build"
+         (info (makecmd <$> argument str idm) idm)
+       )
 
 main :: IO ()
 main = runStake $ \(command:args) -> do
@@ -23,3 +81,5 @@ main = runStake $ \(command:args) -> do
                         askBuiltPackages n
                             $ map PackageName pkgNames
         _ -> error $ "Unknown invocation: " ++ show (command, args)
+
+-}
