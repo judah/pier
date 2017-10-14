900b09dd1852bd68656f7660d71f7f95182dae79
chris mckinlay <cem3394@chriss-MacBook.local>
2017-10-14 12:14:29 -0700

    wip

--- src/Development/Stake.hs
+++ src/Development/Stake.hs
@@ -7,18 +7,51 @@ import Development.Stake.Core
 import Development.Stake.Package
 import Development.Stake.Stackage
 import Distribution.Package
+import Options.Applicative
+
+
+data Command = Clean | CleanAll | Build PlanName PackageName
+
+
+cleanCommand :: Parser Command
+cleanCommand = flag' undefined
+  (  long "clean"
+  <> short 'c'
+  <> help "Clean project" )
+
+cleanAllCommand :: Parser Command
+cleanAllCommand = flag' undefined
+  (  long "clean-all"
+  <> help "Clean project & dependencies" )
+
+buildCommand :: Parser Command
+buildCommand = Build <$> planName <*> packageName
+  where
+    planName = PlanName <$> strOption
+      (  long "build"
+      <> short 'b'
+      <> metavar "PLANNAME"
+      <> help "Build project" )
+    packageName = PackageName <$> strOption
+      (  long "build"
+      <> short 'f'
+      <> metavar "PACKAGENAME"
+      <> help "Build project" )
+
+
+input :: Parser Command
+input = cleanCommand  <|> cleanAllCommand <|> buildCommand
+
+opts :: ParserInfo Command
+opts = info input mempty
+
+runWithOptions :: ParserResult Command -> Rules ()
+runWithOptions = undefined
 
 main :: IO ()
-main = runStake $ \(command:args) -> do
+main = runStake $ \args -> do
     downloadCabalPackageRule
     buildPlanRules
     buildPackageRules
-    case command of
-        "clean" -> cleanBuild
-        "clean-all" -> cleanAll
-        "build"
-            | planName:pkgNames <- args
-                -> action $ do
-                        plan <- readPlan (PlanName planName)
-                        askBuiltPackages plan $ map PackageName pkgNames
-        _ -> error $ "Unknown invocation: " ++ show (command, args)
+    runWithOptions $ execParserPure undefined opts args
+
