830ee80871ff10368080fc76d92af0505469e57b
Judah Jacobson <judah@users.noreply.github.com>
2018-03-03 19:03:50 -0800

    missing modules

---
+++ src/Pier/Build/Executable.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DeriveAnyClass #-}
+module Pier.Build.Executable
+    ( askBuiltExecutable
+    , BuiltExecutableQ(..)
+    , BuiltExecutable(..)
+    , progExe
+    ) where
+
+import Data.Semigroup
+import Data.Set (Set)
+import Development.Shake
+import Development.Shake.Classes
+import Distribution.Package (PackageName)
+import Distribution.Text (display)
+import GHC.Generics (Generic(..))  
+
+import Pier.Core.Artifact
+import Pier.Core.Persistent
+
+data BuiltExecutable = BuiltExecutable
+    { builtBinary :: Artifact
+    , builtExeDataFiles :: Set Artifact
+    } deriving (Show, Eq, Generic, Hashable, Binary, NFData)
+
+
+data BuiltExecutableQ = BuiltExecutableQ PackageName String
+    deriving (Typeable, Eq, Generic, Hashable, Binary, NFData)
+type instance RuleResult BuiltExecutableQ = BuiltExecutable
+
+instance Show BuiltExecutableQ where
+    show (BuiltExecutableQ p e) = "Executable " ++ e ++ " from " ++ display p
+
+askBuiltExecutable :: PackageName -> String -> Action BuiltExecutable
+askBuiltExecutable p e = askPersistent $ BuiltExecutableQ p e
+
+progExe :: BuiltExecutable -> [String] -> Command
+progExe exe args = progA (builtBinary exe) args
+                <> inputs (builtExeDataFiles exe)
---
+++ src/Pier/Build/Flags.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE DeriveAnyClass #-}
+module Pier.Build.Flags
+    ( TransitiveDeps(..)
+    , CFlags(..)
+    , getCFlags
+    , ghcDefines
+    ) where
+
+import Control.Applicative (liftA2)
+import Control.Monad (msum)
+import Data.Semigroup
+import Data.Set (Set)
+import Development.Shake
+import Development.Shake.Classes
+import Distribution.PackageDescription
+import Distribution.Text (display)
+import Distribution.Types.PkgconfigDependency
+import Distribution.Version (versionNumbers)
+import GHC.Generics (Generic(..))
+
+import qualified Data.Set as Set
+
+import Pier.Build.Stackage
+import Pier.Core.Artifact
+
+data TransitiveDeps = TransitiveDeps
+    { transitiveDBs :: Set Artifact
+    , transitiveLibFiles :: Set Artifact
+    , transitiveIncludeDirs :: Set Artifact
+    , transitiveDataFiles :: Set Artifact
+    } deriving (Show, Eq, Typeable, Generic, Hashable, Binary, NFData)
+
+instance Semigroup TransitiveDeps
+
+instance Monoid TransitiveDeps where
+    mempty = TransitiveDeps Set.empty Set.empty Set.empty Set.empty
+    TransitiveDeps dbs files is datas
+        `mappend` TransitiveDeps dbs' files' is' datas'
+        = TransitiveDeps (dbs <> dbs') (files <> files') (is <> is')
+                (datas <> datas')
+
+-- TODO: macros file also
+data CFlags = CFlags
+    { ccFlags :: [String]
+    , cppFlags :: [String]
+    , cIncludeDirs :: Set Artifact
+    , linkFlags :: [String]
+    , linkLibs :: [String]
+    }
+
+getCFlags :: TransitiveDeps -> Artifact -> BuildInfo -> Action CFlags
+getCFlags deps pkgDir bi = do
+    pkgConfFlags <- mconcat <$> mapM getPkgConfFlags (pkgconfigDepends bi)
+    return CFlags
+            { ccFlags = ccOptions bi ++ fst pkgConfFlags
+            , cppFlags = cppOptions bi
+            , cIncludeDirs =
+                    Set.fromList (map (pkgDir />) $ includeDirs bi)
+                    <> transitiveIncludeDirs deps
+            , linkFlags = ldOptions bi ++ snd pkgConfFlags
+            , linkLibs = extraLibs bi
+            }
+
+-- TODO: handle version numbers too
+getPkgConfFlags :: PkgconfigDependency -> Action ([String], [String])
+getPkgConfFlags (PkgconfigDependency name _) = liftA2 (,)
+    (runPkgConfig [display name, "--cflags"])
+    (runPkgConfig [display name, "--libs"])
+  where
+    runPkgConfig = fmap words . runCommandStdout . prog "pkg-config"
+
+-- | Definitions that GHC provides by default
+ghcDefines :: InstalledGhc -> [String]
+ghcDefines ghc = ["-D__GLASGOW_HASKELL__=" ++
+                    cppVersion (ghcInstalledVersion ghc)]
+  where
+    cppVersion v = case versionNumbers v of
+        (v1:v2:_) -> show v1 ++ if v2 < 10 then '0':show v2 else show v2
+        _ -> error $ "cppVersion: " ++ display v
+
+
